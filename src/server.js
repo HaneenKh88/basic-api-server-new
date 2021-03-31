d82ca715b1a343d08406a7903fa34d5db6f79ed5
haneen <haneen.khasawneh96@gmail.com>
2021-03-31 16:56:16 +0300

    test

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 'use strict';
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 // require('dotenv').config();
 
-// const clothRouter = require('./routes/clothes.js');
-// const foodRouter = require('./routes/food.js');
+const clothRouter = require('./routes/clothes.js');
+const foodRouter = require('./routes/food.js');
 
-// const notFoundHndler = require('./error-handlers/404.js');
-// const errorHandler = require('./error-handlers/500.js');
+const notFoundHndler = require('./error-handlers/404.js');
+const errorHandler = require('./error-handlers/500.js');
 
 const app = express();
 
 app.use(express.json());
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(cors());
-// app.use('/api/v1/cloth/', clothRouter);
-// app.use('/api/v1/food/', foodRouter);
+app.use('/api/v1/cloth/', clothRouter);
+app.use('/api/v1/food/', foodRouter);
 
 function homeHandler(req, res) {
     res.send('Hello World');
@@ -25,8 +25,8 @@ function homeHandler(req, res) {
 app.get('/', homeHandler);
 
 
-// app.use('*', notFoundHndler);
-// app.use(errorHandler);
+app.use('*', notFoundHndler);
+app.use(errorHandler);
 
 module.exports =
 {
